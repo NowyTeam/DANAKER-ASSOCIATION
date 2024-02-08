2b0c313ab7cede96d25f97a79c7229a38b4c2705
NowyTeam <jaj2232ajn@gmail.com>
2024-02-08 16:02:06 +0600

    Update

--- src/components/pages/home/Mission/Mission.tsx
+++ src/components/pages/home/Mission/Mission.tsx
@@ -1,4 +1,6 @@
 import Style from "./Mission.module.scss";
+import Photo from "../../../../assets/photo.svg";
+import Accept from "../../../../assets/accept.svg";
 
 const Mission = () => {
   return (
@@ -17,9 +19,38 @@ const Mission = () => {
           </div>
           <div className={Style.main_box}>
             <div className={Style.photos}>
-              <img src="../../../../../First_Grid.jpeg" alt="Loading..." />
-              <img src="../../../../../Second_Grid.jpeg" alt="Loading..." />
-              <img src="../../../../../Thirsd_Grid.jpeg" alt="Loading..." />
+              <img src={Photo} alt="Loading..." />
+            </div>
+            <div className={Style.Fremer_Adapter}>
+              <div className={Style.Title_Box}>
+                <p className={Style.Discaption}>
+                  <img src={Accept} alt="Loading..." />{" "}
+                  <span>Integration:</span> We create a supportive environment
+                  for women from Kyrgyz Republic in Poland, offering resources,
+                  guidance and networking opportunities to facilitate their
+                  smooth integration into local society
+                </p>
+              </div>
+              <div className={Style.Title_Box}>
+                <p className={Style.Discaption}>
+                  <img src={Accept} alt="Loading..." />{" "}
+                  <span>Cultural Heritage:</span> We celebrate and preserve our
+                  cultural traditions and customs through cultural events,
+                  workshops, and storytelling and dialogue sessions, enriching
+                  the diversity of Polish society.
+                </p>
+              </div>
+              <div className={Style.Title_Box}>
+                <p className={Style.Discaption}>
+                  <img src={Accept} alt="Loading..." />{" "}
+                  <span>Empowerment:</span> Our mission centers on empowering
+                  migrant women to realize their full potential in foreign
+                  country personally, professionally, and socially. Through
+                  mentoring and skill-building programs, we equip women with the
+                  tools and confidence to overcome obstacles and make meaningful
+                  contributions to the community and society at large.
+                </p>
+              </div>
             </div>
           </div>
         </div>
