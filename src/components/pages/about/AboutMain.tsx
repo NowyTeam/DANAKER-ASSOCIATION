0c0eee0c29a71394cff76efdb8d6cac605645478
NowyTeam <jaj2232ajn@gmail.com>
2024-02-13 15:48:27 +0600

    Update

--- src/components/pages/about/AboutMain.tsx
+++ src/components/pages/about/AboutMain.tsx
@@ -1,12 +1,14 @@
 // import Style from "./AboutMain.module.scss";
 import First_Page from "./First_About/First_About";
 import Stands from "./Stands_About/Stands";
+import OurTeam from "./Our_Team/OurTeam";
 
 const AboutMain = () => {
   return (
     <>
       <First_Page />
       <Stands />
+      <OurTeam />
     </>
   );
 };
--- src/components/pages/about/Stands_About/Stands.tsx
+++ src/components/pages/about/Stands_About/Stands.tsx
@@ -9,7 +9,9 @@ const Stands = () => {
           <div className={Style.Main_box}>
             <div className={Style.boxes}>
               <div className={Style.Adapter_box}>
-                <h1>Integration</h1>
+                <h1>
+                  <span>01</span>Integration
+                </h1>
                 <p>
                   At Danaker, We Believe In Promoting Integration By Providing A
                   Supportive Environment For Women From Kyrgyzstan In Poland. We
@@ -25,6 +27,50 @@ const Stands = () => {
               </div>
               <div className={Style.Adapter_box}>
                 <img src="../../../../../public/about1.jpeg" alt="" />
+              </div>
+            </div>
+            <div className={Style.boxes}>
+              <div className={Style.Adapter_box}>
+                <h1>
+                  <span>02</span>Cultural Heritage
+                </h1>
+                <p>
+                  At Danaker, We Believe In Promoting Integration By Providing A
+                  Supportive Environment For Women From Kyrgyzstan In Poland. We
+                  Aim To Facilitate Their Smooth Transition Into The Local
+                  Society By Offering Resources, Guidance, And Networking
+                  Opportunities. Through Workshops, Language Classes, And
+                  Cultural Exchange Programs, We Encourage Cross-Cultural
+                  Understanding And Foster Strong Bonds Between The Kyrgyz
+                  Diaspora And The Wider Polish Community. By Promoting
+                  Entegration, We Strive To Create A Harmonious And Inclusive
+                  Society Where Every Women Feels Valued And Connected.
+                </p>
+              </div>
+              <div className={Style.Adapter_box}>
+                <img src="../../../../../public/about2.jpeg" alt="" />
+              </div>
+            </div>
+            <div className={Style.boxes}>
+              <div className={Style.Adapter_box}>
+                <h1>
+                  <span>03</span>Empowerment
+                </h1>
+                <p>
+                  At Danaker, We Believe In Promoting Integration By Providing A
+                  Supportive Environment For Women From Kyrgyzstan In Poland. We
+                  Aim To Facilitate Their Smooth Transition Into The Local
+                  Society By Offering Resources, Guidance, And Networking
+                  Opportunities. Through Workshops, Language Classes, And
+                  Cultural Exchange Programs, We Encourage Cross-Cultural
+                  Understanding And Foster Strong Bonds Between The Kyrgyz
+                  Diaspora And The Wider Polish Community. By Promoting
+                  Entegration, We Strive To Create A Harmonious And Inclusive
+                  Society Where Every Women Feels Valued And Connected.
+                </p>
+              </div>
+              <div className={Style.Adapter_box}>
+                <img src="../../../../../public/about3.jpeg" alt="" />
               </div>
             </div>
           </div>
