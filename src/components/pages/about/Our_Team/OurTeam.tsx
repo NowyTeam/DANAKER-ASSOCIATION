8f0b074f7c27b6b61d7b987512594e744aa6e95c
NowyTeam <jaj2232ajn@gmail.com>
2024-02-13 16:01:23 +0600

    UpgradeGrid

--- src/components/pages/about/Our_Team/OurTeam.tsx
+++ src/components/pages/about/Our_Team/OurTeam.tsx
@@ -105,7 +105,114 @@ const OurTeam = () => {
               </h3>
               <h3>
                 <img src={Accept} alt="" />
-                I’d Like To Contribute To The Development Of Our Ladies By
+                I'd Like To Contribute To The Development Of Our Ladies By
+                Self-Improvement And Sharing My Experience, Knowledge With
+                Others.
+              </h3>
+            </div>
+          </div>
+        </div>
+        <div className={Style.TeamWrapper}>
+          <div className={Style.box}>
+            <img
+              className={Style.Chel}
+              src="../../../../../chel_5.jpeg"
+              alt="Loading..."
+            />
+            <div className={Style.Team_Dis}>
+              <h2>Aitolgon Boronbaeva</h2>
+              <hr />
+              <h3>
+                <img src={Accept} alt="" /> Founder and executive director of
+                “Danaker”
+              </h3>
+              <h3>
+                <img src={Accept} alt="" />
+                Ph.D. Candidate In Political Science
+              </h3>
+              <h3>
+                <img src={Accept} alt="" />
+                Public Representative Of Women From Kyrgyzstan In Poland Under
+                The Council Of Migration Under Kyrgyz Parliament
+              </h3>
+            </div>
+          </div>
+          <div className={Style.box}>
+            <img
+              className={Style.Chel}
+              src="../../../../../chel_6.jpeg"
+              alt="Loading..."
+            />
+            <div className={Style.Team_Dis}>
+              <h2>Begimai Taalaibekova</h2>
+              <hr />
+              <h3>
+                <img src={Accept} alt="" /> Deputy Director Of "Danaker"
+              </h3>
+              <h3>
+                <img src={Accept} alt="" />
+                Management / Business
+              </h3>
+              <h3>
+                <img src={Accept} alt="" />
+                JLL Company-Analyst
+              </h3>
+              <h3>
+                <img src={Accept} alt="" />
+                I'm The Ultimate Researcher In The World Of Random Fun Facts,
+                Ready To Share My Tastes And Smiles With Others!
+              </h3>
+            </div>
+          </div>
+          <div className={Style.box}>
+            <img
+              className={Style.Chel}
+              src="../../../../../chel_7.jpeg"
+              alt="Loading..."
+            />
+            <div className={Style.Team_Dis}>
+              <h2>Akmoor Mazhitova</h2>
+              <hr />
+              <h3>
+                <img src={Accept} alt="" /> Finance Administrator Of "Danaker"
+              </h3>
+              <h3>
+                <img src={Accept} alt="" />
+                Management- Financial Engineering
+              </h3>
+              <h3>
+                <img src={Accept} alt="" />
+                JP. Morgan- Finance Coordinator
+              </h3>
+              <h3>
+                <img src={Accept} alt="" />I Do Investment And Want To
+                Popularise It In Kyrgyzstan
+              </h3>
+            </div>
+          </div>
+          <div className={Style.box}>
+            <img
+              className={Style.Chel}
+              src="../../../../../chel_8.jpeg"
+              alt="Loading..."
+            />
+            <div className={Style.Team_Dis}>
+              <h2>Nuraiym Melisova</h2>
+              <hr />
+              <h3>
+                <img src={Accept} alt="" /> Project Manager At "Danaker"
+              </h3>
+              <h3>
+                <img src={Accept} alt="" />
+                International Business-Timberseed
+              </h3>
+              <h3>
+                <img src={Accept} alt="" />
+                Research Specialist
+              </h3>
+              <h3>
+                <img src={Accept} alt="" />
+                I'd Like To Contribute To The Development Of Our Ladies By
                 Self-Improvement And Sharing My Experience, Knowledge With
                 Others.
               </h3>
