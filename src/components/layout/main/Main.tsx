f887532b64da649e7307c6c5499f02a28e71b345
NowyTeam <jaj2232ajn@gmail.com>
2024-02-14 20:10:52 +0600

    AddEvents

--- src/components/layout/main/Main.tsx
+++ src/components/layout/main/Main.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../../pages/home/Home";
 import About from "../../pages/about/AboutMain";
+import Events from "../../pages/events/Events";
 export const Main = () => {
   return (
     <div>
@@ -8,6 +9,7 @@ export const Main = () => {
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
       </Routes>
     </div>
   );
